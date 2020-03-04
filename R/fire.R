e4d599eb13357532c6387d93e66e7929c46136f2
Nathan <nvanderheiden@ou.edu>
2020-03-04 17:15:08 -0600

    added fire data set

---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' Fire data set
+#'
+#' A data set about the distance from fire station and damage of fires.
+#'
+#' @format A data frame with 15 rows and 2 variabels:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station in miles}
+#'   \item{DAMAGE}{X 1000 dollars in damage}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
